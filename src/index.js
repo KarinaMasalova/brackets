1af5ea68c2f93756aaa6076e1df9918da0be933f
Karina Masalova <masalova.karina@mail.ru>
2020-03-17 22:19:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    const arrayOfBrackets = bracketsConfig.map(item => item.join(''));
+
+    for(let i = 0; i < arrayOfBrackets.length; i++) {
+        if(str.includes(arrayOfBrackets[i])) {
+            str = str.replace(arrayOfBrackets[i], '');
+            i = -1;
+        }
+    }
+
+    return str ? false : true;
 }
